3c37ab120e81f15ef96653d80905c8fe66635957
Preet Karwal <preetverma365@gmail.com>
2025-05-15 14:47:13 +0530

    built redirect in body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,35 @@
-
+import axios from "axios"
+import { useDispatch, useSelector } from 'react-redux'
+import { BASE_URL } from '../utils/constants'
 import Footer from './Footer'
 import NavBar from './NavBar'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
+import {addUser} from "../utils/userSlice.js"
+import { useEffect } from 'react'
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user)
+
+
+  const fetchUser = async () => {     
+    if(userData) return;       // if userData is already present then no more again auth api call
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {withCredentials:true});
+      dispatch(addUser(res.data))
+    } catch (error) {
+        if(error.status === 401){
+          navigate("/login");
+        }
+        console.log(error)
+    }
+  }
+
+  useEffect(()=> {
+    fetchUser();
+  },[])
+
   return (
     <div>
         <NavBar />
