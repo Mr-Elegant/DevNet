408b9b8f8da013284c32196becbd8182257a9091
Preet Karwal <preetverma365@gmail.com>
2025-10-27 11:36:38 +0530

    constant api syntex url correction

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@
 
 // export const BASE_URL = "http://localhost:3000";
 
-export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" : "/apis";
+export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" : "/api";
 
 
 
